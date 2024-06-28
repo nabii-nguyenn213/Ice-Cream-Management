606de52faa60bbbeb3b9da37efd40e5dc2935fa4
nabii-nguyenn213 <nabilearn213@gmail.com>
2024-06-28 23:37:07 +0700

    main

---
+++ main.py
@@ -0,0 +1,56 @@
+from Ice_Cream import *
+
+def employee_menu():
+    print("+------MIXUE ICE-CREAMING SALE MANAGEMENT------+")
+    print("|    1. Show all Ice-cream                     |")
+    print("|    2. Add new Ice-cream                      |")
+    print("|    3. Delete Ice-cream                       |")
+    print("|    4. Modify Ice-cream                       |")
+    print("|    5. Generate Invoice                       |")
+    print("|    6. Add Employee                           |")
+    print("|    7. Delete Employee                        |")
+    print("|    8. Return to Main Menu                    |")
+    print("+----------------------------------------------+")
+    option = int(input("Choose an option (1-8): "))
+    while option < 1 and option > 8:
+        option = int(input("Choose an option (1-8): "))
+    return option
+
+def customer_menu():
+    pass
+
+def choose_menu():
+    print("+------------MENU-------------+")
+    print("|  1. Employee                |")
+    print("|  2. Customer                |")
+    print("+-----------------------------+")
+    option = int(input("Choose an option(1-2): "))
+    while option > 2 or option < 1:
+        option = int(input("Choose an option(1-2): "))
+    return option
+
+def main():
+    option = choose_menu()
+    if option == 1:
+        employee_option = employee_menu()
+        management = Management()
+        if employee_option == 1:
+            management.show_menu()
+        elif employee_option == 2:
+            management.add_new_ice_cream()
+        elif employee_option == 3:
+            pass
+        elif employee_option == 4:
+            management.modify_ice_cream()
+        elif employee_option == 5:
+            pass
+        elif employee_option == 6:
+            pass
+        elif employee_option == 7:
+            pass
+        else:
+            pass
+    else:
+        customer_option = customer_menu()
+
+main()
