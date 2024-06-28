a1af9dda7679e00615aeddb6779e7814e5ef7165
nabii-nguyenn213 <nabilearn213@gmail.com>
2024-06-28 23:36:13 +0700

    ice-cream list

---
+++ List_Of_IceCream.py
@@ -0,0 +1,8 @@
+def read_file():
+    file = open('icecream.txt', 'r')
+    list_of_ice_cream = {}
+    for line in file:
+        l = line.split("-")
+        l[2] = l[2].replace("\n", "")
+        list_of_ice_cream[l[0]] = [l[1], l[2]] # {id: [name, price]}
+    return list_of_ice_cream
